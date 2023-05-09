2adac5f9bd76e130163dc5fcef9d87e07054814b
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-05-09 10:53:36 +0530

    code formating index.js and index.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.css";
-import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <ContextProvider>
