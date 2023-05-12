c2596a1f4c7b04e009a846c5006b927b71ad7e06
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-05-12 11:03:19 +0530

    Charts code formating

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
-import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
+import { PureComponent } from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-class SparkLine extends React.PureComponent {
+class SparkLine extends PureComponent {
   render() {
     const { id, height, width, color, data, type, currentColor } = this.props;
 
@@ -17,12 +21,12 @@ class SparkLine extends React.PureComponent {
         tooltipSettings={{
           visible: true,
           // eslint-disable-next-line no-template-curly-in-string
-          format: '${x} : data ${yval}',
+          format: "${x} : data ${yval}",
           trackLineSettings: {
             visible: true,
           },
         }}
-        markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+        markerSettings={{ visible: ["All"], size: 2.5, fill: currentColor }}
         dataSource={data}
         xName="x"
         yName="yval"
